389f8f8cf3e1c567a1f0ff12bfe241e992dfc5bc
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2026-02-18 18:17:45 +0100

    Package/-s updated to most recent version

--- src/SubspaceFolder.cs
+++ src/SubspaceFolder.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Aspenlaub.Net.GitHub.CSharp.Pegh.Entities;
 using Aspenlaub.Net.GitHub.CSharp.Pegh.Extensions;
 using Aspenlaub.Net.GitHub.CSharp.Pegh.Interfaces;
+using Aspenlaub.Net.GitHub.CSharp.Skladasu.Entities;
+using Aspenlaub.Net.GitHub.CSharp.Skladasu.Extensions;
 
 namespace Aspenlaub.Net.GitHub.CSharp.SubspaceSensor;
 
